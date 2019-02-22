08c17e435b7c36a97c7801806c679c950e641009
Beatriz Piniello <b.piniello@gmail.com>
2019-02-22 16:12:02 +0100

    Cambios en program main

--- program_main.f90
+++ program_main.f90
@@ -1,1 +1,17 @@
 !Programa Principal
+
+
+program main
+implicit none
+integer :: N,Maxstep
+real*8 :: density,cut,L,dt,Temp
+real*8,allocatable :: r(:,:),vel(:,:),force(:,:)
+
+!Parameters
+N=108
+density=0.9
+dt=0.0003
+
+allocate(r(N,3),vel(N,3),force(N,3))
+
+
