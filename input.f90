756ff278c888001cd6be05b3a8c3f0dc4749c973
Encarna Valles <encarna.vct@hotmail.com>
2019-03-03 12:36:26 +0100

    Input files

---
+++ input.f90
@@ -0,0 +1,18 @@
+    subroutine input(M,dt,mass,density,Temp,Maxtime,sigma,eps,nhis)
+        implicit none
+        real*8, parameter :: kb = 1.3806d-23
+        integer           :: M, nhis
+        real*8            :: dt, mass, density, Temp, Maxtime
+        real*8            :: press       !Initial pression
+        real*8            :: sigma
+        real*8            :: eps
+        
+        read(5,*)
+        read(5,*)
+        read(5,*) M, dt, mass, Temp, press, Maxtime, sigma, eps, nhis
+ 
+
+        density = (press/9.869d-6)/(kb*Temp)
+        density = density * sigma**3d0 !Reduced units
+        Temp = (kb*Temp*6.022d23)/eps  !Reduced units
+    end subroutine input
