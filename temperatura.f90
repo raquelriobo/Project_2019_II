80476194091d8b662dfccabe0a3066bcaee433a7
Encarna Valles <encarna.vct@hotmail.com>
2019-03-03 11:15:45 +0100

    Indents

--- temperatura.f90
+++ temperatura.f90
@@ -1,9 +1,9 @@
-        SUBROUTINE TEMPERATURA(e_kin,n_part,temp)
-            IMPLICIT NONE
-            REAL(8), INTENT(IN)    :: e_kin
-            REAL(8), INTENT(OUT)   :: temp
-            INTEGER, INTENT(IN)    :: n_part
+    SUBROUTINE TEMPERATURA(e_kin,n_part,temp)
+        IMPLICIT NONE
+        REAL(8), INTENT(IN)    :: e_kin
+        REAL(8), INTENT(OUT)   :: temp
+        INTEGER, INTENT(IN)    :: n_part
             
-            temp = (2.d0*e_kin)/(3.d0*n_part)
+        temp = (2.d0*e_kin)/(3.d0*n_part)
 
-        END SUBROUTINE TEMPERATURA
+    END SUBROUTINE TEMPERATURA
